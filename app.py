def9ddc905c5bb9fee10660f8032886420e0de26
chrismountzouris <chrismountzou@gmail.com>
2023-03-01 12:08:03 +0200

    - Add dashboard .html files with the subcomponents
    - Implementation of metabolic rate detection
    
    - Minor fixes on navbar menu

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import (Flask, render_template, redirect, url_for, request)
 from flask_mysqldb import MySQL
 
+from determineMetabolic import dailyMetabolic
+
 app = Flask(__name__)
 
 # Credentials to connect with mySQL DB of CDT UPAT
@@ -15,6 +17,24 @@ mysql = MySQL(app)
 @app.route("/")
 @app.route("/index/")
 def rout():
+
+    cur = mysql.connection.cursor()
+
+    # Execute SQL query to get the average environmental parameters of temperature and humidity that recorded during the last 24-hours
+    cur.execute('''SELECT ROUND(AVG(tc_temperature),2), ROUND(AVG(tc_humidity), 2) FROM user_thermal_comfort WHERE tc_timestamp >= UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 24 HOUR))''')
+    (avg_temperature, avg_humidity) = cur.fetchone()
+
+    # Execute SQL query to get the latest environmental parameters of temperature and humidity that recorded
+    cur.execute('''SELECT tc_temperature, tc_humidity FROM user_thermal_comfort ORDER BY tc_timestamp DESC LIMIT 1;''')
+    (latest_temperature, latest_humidity) = cur.fetchone()
+
+    cur.execute('''SELECT tc_metabolic, tc_timestamp FROM user_thermal_comfort WHERE tc_timestamp >= UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 24 HOUR));''')
+    latest_metabolic = cur.fetchall()
+
+    sessions_met = dailyMetabolic(latest_metabolic)
+
+    print(sessions_met)
+
     return render_template("index.html")
 
 
---
+++ determineMetabolic.py
@@ -0,0 +1,22 @@
+# Implement a function with input the calories of the last 24 hours and returns the met values of each session
+def dailyMetabolic(latest_metabolic):
+    sessions = [[]]
+    sessions_met = []
+
+    # Detect each specific session of the last 24 hours
+    for obs, ts in latest_metabolic:
+        if sessions[-1] and obs < sessions[-1][-1][0]:
+            sessions.append([])
+        sessions[-1].append((obs, ts))
+
+    # Specify the metabolic rate and the corresponding timestamp difference of each session
+    for session in sessions:
+        start_ts, start_obs = session[0][1], session[0][0]
+        end_ts, end_obs = session[-1][1], session[-1][0]
+        sessions_met.append(round((end_obs * 1000) / (25 * (end_ts - start_ts)),2))
+        print(f"Session from {start_ts} ({start_obs}) to {end_ts} ({end_obs})")
+
+    # Replace met with the min and max acceptable values in case of less or higher than the corresponding boundary ones
+    sessions_met = [0.9 if x < 0.9 else 2.0 if x > 2.0 else x for x in sessions_met]
+
+    return sessions_met
