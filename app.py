4e55f69fb38eb57be8dce9acce8a7d52cbd8de5d
gregorypro1 <49030480+gregorypro1@users.noreply.github.com>
2023-05-11 18:14:53 +0300

    - change of initialization scheme (debug)

--- app.py
+++ app.py
@@ -370,10 +370,6 @@ def handle_ttn_webhook():
     insert_sql = f"INSERT INTO user_thermal_comfort (tc_temperature, tc_humidity, tc_metabolic, tc_met, tc_clo, tc_comfort, tc_timestamp, wearable_id, gateway_id, wb_index) VALUES ({tc_temperature}, {tc_humidity}, {tc_metabolic}, {tc_met}, {tc_clo}, {tc_comfort}, {tc_timestamp}, '{device_id}', '{gateway_id}', '{wb_index}')"
 
     g.cur.execute(insert_sql)
-    exclude_count = messages2exclude
-    g.cur.execute(
-        f"UPDATE exc_assist SET exclude_counter = {exclude_count}, time_st={tc_timestamp} WHERE wearable_id = %s", (
-            device_id,))
 
     mysql.connection.commit()
     g.cur.close()
