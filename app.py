e29ab32fc8c17a9772c87443d5b8232dbc5070b1
gregorypro1 <49030480+gregorypro1@users.noreply.github.com>
2024-11-14 15:46:24 +0200

    -TCslider

--- app.py
+++ app.py
@@ -385,7 +385,8 @@ def handle_webhk():
         pm10_nconcentration = int(mac_payload[57:60], 16)
         typical_particle = int(mac_payload[61:64], 16)
         if device_id == "0080E1150510BDE6" or device_id == "0080E1150533F233" or device_id == "0080E1150510B77B" or device_id == "0080E1150510B55F" or device_id == "0080E1150533ECF3":
-            url = "https://script.google.com/macros/s/AKfycbzOPn4VcDAs41g2C0vMr5oOxm38okSnpaSMkAS8xfmjVhQmMBqACcKfOjhnrJxRJvZwUA/exec"
+            # url = "https://script.google.com/macros/s/AKfycbzOPn4VcDAs41g2C0vMr5oOxm38okSnpaSMkAS8xfmjVhQmMBqACcKfOjhnrJxRJvZwUA/exec"
+            url = "https://script.google.com/macros/s/AKfycbzOTRNoWvsTMfdDed0XF7P-6HTGTNhFE4-s7ogwXTI-h4fCjYKDvgqUuoPqGtV-nEFT/exec"
         else:
             url = "https://script.google.com/macros/s/AKfycbzxTm-_PNSkPRocRp4Xh3BHm9R0ZsbSXLQ5rARTAZRQlmeAgTF5hjERSy_sFfydktbi/exec"
 
